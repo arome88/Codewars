81b8adbe8c56ea0a5053d4eef52afa038f32eea4
arome88 <anaromero.88@icloud.com>
2023-03-21 01:36:21 -0400

    codewars

---
+++ function1-helloWorld.js
@@ -0,0 +1,8 @@
+//Make a simple function called greet that returns the most-famous "hello world!"
+
+function greet() {
+    const greeting = "hello world!"; //creates a constant variable called 'greeting' and assigns it the string value of "hello world!". The 'const' keyword is used to declare a variable that cannot be reassigned later.
+    return greeting; // returns the value of the 'greeting' variable as the output of the 'greet' function. When the function is called, this value will be returned to the code that called the function
+  }
+
+  // the 'greet' function simply returns the string "hello world!" when called
