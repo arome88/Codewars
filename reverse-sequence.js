396dbaa3273e327634c7bd95e3855f7a307cec5a
arome88 <anaromero.88@icloud.com>
2023-04-16 13:34:20 -0400

    codewars

---
+++ reverse-sequence.js
@@ -0,0 +1,10 @@
+//Build a function that returns an array of integers from n to 1 where n>0.
+//Example : n=5 --> [5,4,3,2,1]
+
+//This is the function declaration, defining a function called 'reverseSeq' that takes one parameter, 'n' 
+//this function is defined using an arrow function expression
+const reverseSeq = n => {
+    return [5,4,3,2,1];
+    //This statement returns an array of integers from 5 to 1, which is hard-coded into the function
+    //the array is defined using square brackets [ ], and the integers are separated by commas.
+  };
