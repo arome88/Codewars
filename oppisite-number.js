c36fdf3e4df4058c960aba1ce57b65c6791f0e22
arome88 <anaromero.88@icloud.com>
2023-04-17 13:20:46 -0400

    codewars

--- oppisite-number.js
+++ oppisite-number.js
@@ -1,5 +1,10 @@
 // Very simple, given an integer or a floating-point number, find its opposite.
 
+//This is the function declaration, defining a function called 'opposite' that takes one parameter, 'number'
 function opposite(number) {
     return -number;
+    //This statement returns the opposite value of the input 'number', which is achieved by multiplying the number by -1
+    //the negative sign '-' in front of the 'number' parameter does the job of multiplying the number by -1
   } 
+
+  //This function can be useful in various mathematical operations where the opposite value of a number is required
