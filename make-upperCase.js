c05cae292f7497ca172ac7d7eb2f04c30b62107d
arome88 <anaromero.88@icloud.com>
2023-04-19 13:15:55 -0400

    codewars

---
+++ make-upperCase.js
@@ -0,0 +1,14 @@
+// Write a function which converts the input string to uppercase.
+
+
+//This is the function declaration, defining a function called 'makeUpperCase' that takes one parameter, 'str'
+function makeUpperCase(str) {
+    return str.toUpperCase(); 
+    //This statement returns the input string 'str' converted to all uppercase characters using the 'toUpperCase()' method
+    //the 'toUpperCase()' method is a built-in JavaScript function that returns a new string with all alphabetic characters converted to uppercase.
+  }
+
+  //the 'makeUpperCase' function takes a string as input and returns the same string with all characters converted to uppercase
+  //This function can be useful, for example, when you need to ensure that a string is in uppercase format for comparison or formatting purposes.
+
+  //For example, if the input string is "hello", the output of this statement would be "HELLO".
