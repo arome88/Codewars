3086e63c2f6e48a5e7eb87fb90a6d2a254522839
arome88 <anaromero.88@icloud.com>
2023-04-24 12:42:28 -0400

    codewars

--- making-zeros-to-the-end.js
+++ making-zeros-to-the-end.js
@@ -7,15 +7,17 @@
     //The line below is the function signature that defines a new function called 'moveZeros' which takes an array 'arr' as input
     function moveZeros(arr) {
 
-    //The nxt 2 lines create two empty arrays to hold the non-zero elements and the zeros, respectively
-    //We will use these arrays to store the elements of 'arr' based on whether they are zero or non-zero
+    //The nxt 2 lines create 2 empty arrays to hold the non-zero elements and the zeros
+    //We will use these arrays to store the elements of 'arr' based on whether they are zero/non-zero
     const nonZeros = [];
     const zeros = [];
 
     //This is a 'for' loop that iterates over each element of the input array 'arr'
     for (let i = 0; i < arr.length; i++) {
 
-    //Inside the loop, we check whether the current element is zero or non-zero. If it is zero, we append it to the zeros array. Otherwise, we append it to the nonZeros array.
+    //Inside the loop, we check whether the current element is zero or non-zero
+    //If it is zero, we append it to the zeros array
+    //Otherwise, we append it to the nonZeros array.
     if (arr[i] === 0) {
     zeros.push(arr[i]);
     } else {
@@ -23,8 +25,10 @@
     }
     }
 
-    //we combine 'nonZeros' and 'zeros' arrays, which effectively moves all the zeros to the end of the array while preserving the order of the other elements. We then return the concatenated array as the output of the function
+    //we combine 'nonZeros' and 'zeros' arrays, which effectively moves all the zeros to the end of the array while preserving the order of the other elements
+    //We then return the combined array as the output of the function
     return nonZeros.concat(zeros);
     }
     
-    console.log(moveZeros([false,1,0,1,2,0,1,3,"a"])); // Output: [false, 1, 1, 2, 1, 3, "a", 0, 0]
+    console.log(moveZeros([false,1,0,1,2,0,1,3,"a"])); 
+    // Output: [false, 1, 1, 2, 1, 3, "a", 0, 0]
